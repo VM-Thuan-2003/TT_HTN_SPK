f9af01a0f2ee8ba71767f0c3be41ef1ad29514d3
vmthuan <vmthuan16052003@gmail.com>
2024-01-17 20:59:54 +0700

    update code lesson week 2

---
+++ LESSON_WEEK_2/BLUETOOTH/WINDOW/main.py
@@ -0,0 +1,19 @@
+import socket
+
+client = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client.connect(("B8:27:EB:0E:F2:0D",1))
+
+try:
+    while True:
+        msg = input("Enter msg: ...")
+        client.send(str(msg).encode("utf-8"))
+        data = client.recv(1024)
+        if not data:
+            break
+            
+        print(f"msg: {data.decode('utf-8')}")
+
+except OSError as e:
+    pass
+
+client.close()
---
+++ LESSON_WEEK_2/README.md
@@ -0,0 +1,6 @@
+## This is document for LESSON_WEEK_2
+
+# For Bluetooth
+
+
+# For Wifi

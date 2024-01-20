9ddb40f23fb86f0e095796314582978f038e5c60
VM_THUAN_2003 <vmthuan16052003@gmail.com>
2024-01-20 11:43:56 +0700

    update code lesson week 2

--- LESSON_WEEK_2/WIFI/RASP/WebServer/main.py
+++ LESSON_WEEK_2/WIFI/RASP/WebServer/main.py
@@ -1,6 +1,29 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
+import RPi.GPIO as GPIO
+
+led_1  = 2
+led_2  = 3
+
 PORT = 5050
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_1,GPIO.OUT)
+GPIO.setup(led_2,GPIO.OUT)
+GPIO.output(led_1, 0)
+GPIO.output(led_2, 0)
+
+def control_light_inv():
+    if(GPIO.input(led_1) == 1):
+        GPIO.output(led_1, 0)
+    else:
+        GPIO.output(led_1, 1)
+
+    if(GPIO.input(led_2) == 1):
+        GPIO.output(led_2, 0)
+    else:
+        GPIO.output(led_2, 1)
 
 class Sever(BaseHTTPRequestHandler):
 
@@ -9,18 +32,23 @@ class Sever(BaseHTTPRequestHandler):
             self.path = '/index.html'
         if self.path == '/led/1/on':
             print("led 1 on")
+            GPIO.output(led_1, 1)
             self.path = '/index.html'
         if self.path == '/led/1/off':
             print("led 1 off")
+            GPIO.output(led_1, 0)
             self.path = '/index.html'
         if self.path == '/led/2/on':
             print("led 2 on")
+            GPIO.output(led_2, 1)
             self.path = '/index.html'
         if self.path == '/led/2/off':
             print("led 2 off")
+            GPIO.output(led_2, 0)
             self.path = '/index.html'
         if self.path == '/led/inv':
             print("led inv")
+            control_light_inv()
             self.path = '/index.html'
         try:
             file_to_open = open(self.path[1:]).read()
@@ -37,6 +65,7 @@ def main():
         server.serve_forever()
     except KeyboardInterrupt:
         server.socket.close()
+        GPIO.cleanup()
         
 if __name__ == __name__:
     print("__start__")

4fcc33a2332760cb97f7074002734cd788ceca0f
vmthuan <vmthuan16052003@gmail.com>
2024-01-31 18:07:45 +0700

    update code lesson week 2

--- LESSON_WEEK_2/BLUETOOTH/WINDOW/app.py
+++ LESSON_WEEK_2/BLUETOOTH/WINDOW/app.py
@@ -11,7 +11,8 @@ class BluetoothController:
         self.client_socket = None
 
     def connect(self):
-        self.client_socket = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+        self.client_socket = socket.socket(
+            socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
         self.client_socket.connect((self.host, self.port))
 
     def send_command(self, command):
@@ -30,41 +31,48 @@ class BluetoothController:
                 print(f"msg: {data}")
                 return data
             return None
+
+
 class App:
     def __init__(self, root):
         self.root = root
         self.root.geometry("500x400")
         self.root.title("Ứng dụng kết nối Bluetooth")
 
-        self.controller = BluetoothController("B8:27:EB:0E:F2:0D", 1)  # Thay đổi địa chỉ MAC và cổng theo yêu cầu
+        # Thay đổi địa chỉ MAC và cổng theo yêu cầu
+        self.controller = BluetoothController("DC:A6:32:11:2A:6E", 1)
 
-        self.connect_button = ttk.Button(root, text="Connect", command=self.connect)
+        self.connect_button = ttk.Button(
+            root, text="Connect", command=self.connect)
         self.connect_button.place(x=50, y=50)
 
-        self.disconnect_button = ttk.Button(root, text="Disconnect", command=self.disconnect, state=tk.DISABLED)
+        self.disconnect_button = ttk.Button(
+            root, text="Disconnect", command=self.disconnect, state=tk.DISABLED)
         self.disconnect_button.place(x=150, y=50)
 
         self.command_entry = ttk.Entry(root, width=20)
         self.command_entry.place(x=250, y=50)
 
-        self.send_button = ttk.Button(root, text="Send Command", command=self.send_command_entry, state=tk.DISABLED)
+        self.send_button = ttk.Button(
+            root, text="Send Command", command=self.send_command_entry, state=tk.DISABLED)
         self.send_button.place(x=400, y=50)
 
-        self.label_log_int = ttk.Label(root, text = "Log: ")
+        self.label_log_int = ttk.Label(root, text="Log: ")
         self.label_log_int.place(x=250, y=200)
-        
-        self.label_log = ttk.Label(root, text = " ....")
+
+        self.label_log = ttk.Label(root, text=" ....")
         self.label_log.place(x=280, y=200)
-        
+
         self.layout_main()
         self.info()
+
     def connect(self):
         self.controller.connect()
         self.connect_button.configure(state=tk.DISABLED)
         self.disconnect_button.configure(state=tk.NORMAL)
         self.send_button.configure(state=tk.NORMAL)
         self.label_log.config(text="Connected")
-        
+
     def disconnect(self):
         self.controller.disconnect()
         self.connect_button.configure(state=tk.NORMAL)
@@ -95,33 +103,44 @@ class App:
 
     def led_inv(self):
         self.send_command("led_inv")
+
     def info(self):
-        label_1 = ttk.Label(self.root, foreground="red", text = "Bài tập: Ứng dụng điều khiển led có đảo chiều")
-        label_1.place(x=50,y=250)
-        label_1.config(font =("Courier", 11))
-        label_2 = ttk.Label(self.root, text = "Tên nhóm: 3 con báo")
-        label_2.place(x=50,y=274)
-        label_2.config(font =("Courier", 14))
-        label_3 = ttk.Label(self.root, text = "Thành viên 1: Võ Minh Thuận - MSSV: 21161366")
-        label_3.place(x=50,y=300)
-        label_4 = ttk.Label(self.root, text = "Thành viên 2: Lê Quang Thương - MSSV: 21161363")
-        label_4.place(x=50,y=320)
-        label_5 = ttk.Label(self.root, text = "Thành viên 3: Trần Thị Xuân Hy - MSSV: 21161323")
-        label_5.place(x=50,y=340)
+        label_1 = ttk.Label(self.root, foreground="red",
+                            text="Bài tập: Ứng dụng điều khiển led có đảo chiều")
+        label_1.place(x=50, y=250)
+        label_1.config(font=("Courier", 11))
+        label_2 = ttk.Label(self.root, text="Tên nhóm: 3 con báo")
+        label_2.place(x=50, y=274)
+        label_2.config(font=("Courier", 14))
+        label_3 = ttk.Label(
+            self.root, text="Thành viên 1: Võ Minh Thuận - MSSV: 21161366")
+        label_3.place(x=50, y=300)
+        label_4 = ttk.Label(
+            self.root, text="Thành viên 2: Lê Quang Thương - MSSV: 21161363")
+        label_4.place(x=50, y=320)
+        label_5 = ttk.Label(
+            self.root, text="Thành viên 3: Trần Thị Xuân Hy - MSSV: 21161323")
+        label_5.place(x=50, y=340)
 
     def layout_main(self):
-        B11 = ttk.Button(self.root, text="LED 1 ON ", width=20, command=self.led_1_on)
+        B11 = ttk.Button(self.root, text="LED 1 ON ",
+                         width=20, command=self.led_1_on)
         B11.place(x=50, y=100)
-        B12 = ttk.Button(self.root, text="LED 1 OFF", width=20, command=self.led_1_off)
+        B12 = ttk.Button(self.root, text="LED 1 OFF",
+                         width=20, command=self.led_1_off)
         B12.place(x=250, y=100)
 
-        B21 = ttk.Button(self.root, text="LED 2 ON ", width=20, command=self.led_2_on)
+        B21 = ttk.Button(self.root, text="LED 2 ON ",
+                         width=20, command=self.led_2_on)
         B21.place(x=50, y=150)
-        B22 = ttk.Button(self.root, text="LED 2 OFF", width=20, command=self.led_2_off)
+        B22 = ttk.Button(self.root, text="LED 2 OFF",
+                         width=20, command=self.led_2_off)
         B22.place(x=250, y=150)
 
         B3 = ttk.Button(self.root, text="INV", width=20, command=self.led_inv)
         B3.place(x=50, y=200)
+
+
 if __name__ == "__main__":
     root = tk.Tk()
     app = App(root)

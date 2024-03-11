70fa87cb9167db01b5ef7a9bea56b985e16d6289
VM-Thuan-2003 <vmthuan16052003@gmail.com>
2024-03-12 00:30:54 +0700

    update code project final

--- PROJECT_CK/CODE/CODE_ARDUINO/CODE_ARDUINO.ino
+++ PROJECT_CK/CODE/CODE_ARDUINO/CODE_ARDUINO.ino
@@ -2,153 +2,130 @@
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 
-#define pin_servo A0
-#define pin_hr    A1
-#define pin_pir_1 A2
-#define pin_pir_2 A3
+#define pinServo  A0
+#define pinPir_1  A1
+#define pinPir_2  A2
+#define pinHr     A3
+#define pinModeSw A3
 
-const int goc_servo[2] = {0,90};
+#define modeRa  0
+#define modeVao 1
+
+#define sl_xe_max 4
+
+const int goc_servo[2] = { 0, 90 };
 
 LiquidCrystal_I2C lcd(0X27, 16, 2);
 
-Servo myservo; 
+Servo myservo;
 
-unsigned long prev_time = 0;
-int time_delay = 0;
+bool state_temp_1time_1, state_temp_1time_2, state_temp_1time_3;
+int stateMode_prev = 2;
+bool state_read_vao, state_read_ra;
+bool state_read_done_vao, state_read_done_ra;
+bool state_read_rfid;
 
-bool state_temp_1 = false;
-bool state_temp_2 = false;
-bool state_temp_3 = false;
+int sl_xe_curr = 0; 
 
-bool state_read_rfid = false;
-
-bool state_read_checkin = false;
-bool state_read_checkout = false;
-
-bool state_read_done_checkin = false;
-bool state_read_done_checkout = false;
-
-bool state_servo_on = false;
-
-void setup() {
+void setup(){
   Serial.begin(9600);
   
-  pinMode(pin_hr, INPUT);
-  pinMode(pin_pir_1, INPUT);
-  pinMode(pin_pir_2, INPUT);
-  
-  state_all_reset();
+  pinMode(pinHr,    INPUT);
+  pinMode(pinPir_1, INPUT);
+  pinMode(pinPir_2, INPUT);
+  pinMode(pinModeSw,  INPUT);
 
-  myservo.attach(pin_servo);
+  myservo.attach(pinServo);
   myservo.write(goc_servo[0]);
 
   lcd.init();
   lcd.backlight();
-
   lcd.setCursor(0, 0);
   lcd.print("TT_HTN - PROJECT");
   lcd.setCursor(0, 1);
   lcd.print(" NHOM 3 CON BAO ");
-
-  delay(1000);
+  delay(2000);
   lcd.clear();
-  
+
+  sl_xe_curr = 0;
+
+  stateMode_prev = 2;
+  state_temp_1time_1 = false; state_temp_1time_2 = false; state_temp_1time_3 = false;
+  state_read_vao = false, state_read_ra = false;
+  state_read_done_vao = false, state_read_done_ra = false;
+  state_read_rfid = false;
+
   Serial.println("__Arduino_start__");
 }
 
-void loop() {
+void loop(){
   /*
-    state_pir_1 -> check in
-    state_pir_2 -> check out
+    statePir_1 -> vao
+    statePir_2 -> ra
   */
-  bool state_hr    = digitalRead(pin_hr)    == 1 ? true : false;
-  bool state_pir_1 = digitalRead(pin_pir_1) == 1 ? true : false;
-  bool state_pir_2 = digitalRead(pin_pir_2) == 1 ? true : false;
+  bool stateHr = digitalRead(pinHr) == 1 ? true : false;
+  bool statePir_1 = digitalRead(pinPir_1) == 1 ? true : false; 
+  bool statePir_2 = digitalRead(pinPir_2) == 1 ? true : false;
+  bool stateMode = digitalRead(pinModeSw) == 1 ? modeRa : modeVao;
 
-
-  if(state_read_rfid == false)
-    if(state_read_done_checkin == false && state_read_done_checkout == false){
-      
-      if(state_pir_1 == true){ // checkin
-        if(state_temp_1 == false){
-            state_temp_1 = true;
-            state_read_checkin = true;
-            state_read_checkout = false;
-        }
-      }
-      
-      if(state_pir_2 == true){ // checkout
-          if(state_temp_2 == false){
-              state_temp_2 = true;
-              state_read_checkin = false;
-              state_read_checkout = true;
-          }
+  if(stateMode != stateMode_prev){
+    stateMode_prev = stateMode;
+    reset_all_state();
+    Serial.println(stateMode == 1 ? "modeRa" : "modeVao");
+  }
+  
+  if(stateMode == modeVao){
+    // handle some statements about session input gate
+    if(state_temp_1time_1 == false){
+      state_temp_1time_1 = true;
+      lcd_log(3,"Gui Xe Vao",4, "Quet The");
+    }
+    if(statePir_1 == true && state_read_done_vao == false){
+      if(state_temp_1time_2 == false){
+        state_temp_1time_2 = true;
+        state_read_vao = true;
+        lcd_log(4,"Xin Chao",2, "Quet The Vao");
+        Serial.println("ready_input_gate");
       }
     }
-    else{
-      // statement when state_read_done_checkin == true || state_read_done_checkin == true
+    if(state_read_vao == true){
       
     }
+  }
   else{
-      // statement when state_read_rfid == true
-      
-  }
-
-  if(state_read_checkin == true || state_read_checkout == true){
-      if(time_delay <= 20){
-        time_delay += delay_1s();
-      }
-      else{
-        time_delay = 0;
-        state_all_reset();  
-      }
-  }
-
-  Serial.print(String(state_hr));                 Serial.print(";");
-  Serial.print(String(state_pir_1));              Serial.print(";");
-  Serial.print(String(state_pir_2));              Serial.print(";");
-  Serial.print(String(state_read_checkin));       Serial.print(";");
-  Serial.print(String(state_read_checkout));      Serial.print(";");
-  Serial.print(String(state_read_done_checkin));  Serial.print(";");
-  Serial.print(String(state_read_done_checkout)); Serial.print(";");
-  Serial.print(String(state_read_rfid));          Serial.print(";");
-  Serial.print(String(time_delay));               Serial.print(";");
-  Serial.println("");
-  
-}
-
-bool delay_1s(){
-  unsigned long curr_time = millis();
-  if(curr_time - prev_time >= 1000){
-    prev_time = curr_time;
-    return true;  
-  }
-  else return false;
-}
-
-void state_all_reset(){
-  bool state_temp_1 = false;
-  bool state_temp_2 = false;
-  bool state_temp_3 = false;
-  
-  bool state_read_rfid = false;
-  
-  bool state_read_checkin = false;
-  bool state_read_checkout = false;
-  
-  bool state_read_done_checkin = false;
-  bool state_read_done_checkout = false;
-}
-
-String read_data_serial(){
-    if (Serial.available() > 0){
-        String receivedChar = Serial.readStringUntil('\n');
-        if(receivedChar == "__rasp_start__")
-          Serial.println("__rasp_start__");
-          lcd.setCursor(0, 0);
-          lcd.print("__rasp_start__");
-          delay(2000);
-          lcd.clear();
-        return receivedChar;
+    // handle some statements about session output gate
+    if(state_temp_1time_1 == false){
+      state_temp_1time_1 = true;
+      lcd_log(3,"Lay Xe Ve",4, "Quet The");
+      Serial.println("ready_output_gate");
     }
+    if(statePir_2 == true && state_read_done_ra == false){
+      if(state_temp_1time_2 == false){
+        state_temp_1time_2 = true;
+        state_read_ra  = true;
+        lcd_log(4,"Xin Chao",2, "Quet The Ra");
+        Serial.println("ready_output_gate");
+      }
+    }
+  }
+}
+
+void lcd_log(int x1, String dt1, int x2, String dt2){
+  lcd.clear();
+  lcd.setCursor(x1, 0); lcd.print(dt1);
+  lcd.setCursor(x2, 1); lcd.print(dt2);
+}
+
+void reset_all_state(){
+  state_temp_1time_1 = false; state_temp_1time_2 = false; state_temp_1time_3 = false;
+  state_read_vao = false, state_read_ra = false;
+  state_read_done_vao = false, state_read_done_ra = false;
+  state_read_rfid = false;
+}
+
+String read_data_serial() {
+  if (Serial.available() > 0) {
+    String receivedChar = Serial.readStringUntil('\n');
+    return receivedChar;
+  }
 }
